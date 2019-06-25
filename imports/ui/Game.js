51f0ad57c3e935a7450ca4c1517ba04d101a6a78
Tom Segarra <tomsegarra@gmail.com>
2019-06-25 14:29:43 -0700

    WIP: refactor game class

--- imports/ui/Game.js
+++ imports/ui/Game.js
@@ -1,75 +1,147 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import Player from './Player.js';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Players } from '../api/players.js';
 
-export default class Game extends Component {
+class Game extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      youHaveAnswered: this.props.answer != '',
-      answer: this.props.me.answer,
+      youHaveAnswered: false,
+      answer: '',
+      currentUser: null,
+      gameBegun: false,
+      turnId: 0,
     };
+
+    this.setUser = this.setUser.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.persistAnswer = this.persistAnswer.bind(this);
+    this.logout = this.logout.bind(this);
+    this.nextTurn = this.nextTurn.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange() {
-    const answer = ReactDOM.findDOMNode(this.refs.answer).value.trim();
-
-    this.setState({ answer: answer });
+  nextTurn() {
+    this.setState((state) => {
+      return {
+        turnId: (state.turnId + 1) % this.props.players.length
+      }
+    });
   }
 
-  handleSubmit(event) {
+  setUser(user) {
+    this.setState({
+      currentUser: user,
+    });
+  }
+
+  startGame() {
+    this.setState({ gameBegun: true });
+  }
+
+  logout() {
+    this.setState({ currentUser: null });
+  }
+
+  handleChange(event) {
+    this.setState({ answer: event.target.value });
+  }
+
+  handleUserSubmit(event) {
     event.preventDefault();
 
-    const answer = ReactDOM.findDOMNode(this.refs.answer).value.trim();
+    const newUserName = ReactDOM.findDOMNode(this.refs.newUserName).value.trim();
 
-    if (answer != '') {
-      this.props.setAnswer(this.props.me, answer);
-      this.setState({ youHaveAnswered: true, answer: answer });
-    }
+    Players.insert({
+      name: newUserName,
+      score: 0,
+    });
+
+    ReactDOM.findDOMNode(this.refs.newUserName).value = '';
+  }
+
+  deletePlayer(id) {
+    Players.remove(id);
+  }
+
+  persistAnswer() {
+    Players.update(this.state.currentUser._id, {
+      $set: { answer: this.state.answer },
+    });
   }
 
   render() {
-    const playerList = this.props.players.map((player) =>
-      <Player
-        key={player._id}
-        player={player}
-        turn={this.props.turn==player.name}
-        me={this.props.me.name==player.name} />
-    );
+    if (this.state.currentUser && this.state.gameBegun) {
+      const playerList = this.props.players.map((player) =>
+        <li key={player._id} className={ this.state.currentUser._id == player._id ? 'me' : '' }>
+          {player.name} ({player.score})
+          [{player.answer}]
+        </li>
+      );
 
-    return (
-      <div className="game-container">
-        <h3>{this.props.turn}'s turn</h3>
+      return (
+        <div className="game-container">
+          <h3>{this.props.players[this.state.turnId].name}'s turn</h3>
 
-        <div className="card">
-          <p>I would go back to school for ______.</p>
-        </div>
-
-        { !this.state.youHaveAnswered ?
-          <form onSubmit={this.handleSubmit.bind(this)}><input
-            ref="answer"
-            value={this.state.answer}
-            onChange={this.handleChange.bind(this)}
-            placeholder={this.props.turn==this.props.me.name ? "Type your answer here" : "Type an answer for " + this.props.turn + " here" } />
-          <input type="submit" value="Save" /></form> :
-          <div>
-            <h3>Answer: {this.state.answer}</h3>
-            <button onClick={() => {this.setState({ youHaveAnswered: false }); }}>Edit Answer</button>
+          <div className="card">
+            <p>I would go back to school for ______.</p>
           </div>
-        }
 
-        <p>Still waiting for answers from Tom, Ryan, and Ajma.</p>
+          { !this.state.youHaveAnswered ?
+            <div><input
+              value={this.state.answer}
+              onChange={this.handleChange}
+              placeholder={this.props.players[this.state.turnId]==this.state.me ? "Type your answer here" : "Type an answer for " + this.props.players[this.state.turnId].name + " here" }
+              />
+            <button onClick={this.persistAnswer}>Save</button></div> :
+            <div>
+              <h3>Answer: {this.state.answer}</h3>
+              <button onClick={() => {this.setState({ youHaveAnswered: false }); }}>Edit Answer</button>
+            </div>
+          }
 
-        <h3>Score</h3>
-        <ul>
-          {playerList}
-        </ul>
+          <p>Still waiting for answers from Tom, Ryan, and Ajma.</p>
 
-        <button onClick={this.props.nextTurn}>Next turn</button>
-        <button onClick={this.props.logout}>Log out</button>
-      </div>
-    );
+          <h3>Score</h3>
+          <ul>
+            {playerList}
+          </ul>
+
+          <button onClick={this.nextTurn}>Next turn</button>
+          <button onClick={this.logout}>Log out</button>
+        </div>
+      );
+    } else {
+      const playerListItems = this.props.players.map((player) => 
+        <li key={player._id}>
+          <button onClick={() => {this.setUser(player)}}>{player.name}</button>
+          { this.state.currentUser && this.state.currentUser.name == player.name ? <span>!</span> : '' }
+          <button onClick={() => {this.deletePlayer(player._id)}}>&times;</button>
+        </li>
+      );
+
+      return (
+        <div className="login-container">
+          <h1>Select your name, or add new</h1>
+          <ul>
+            {playerListItems}
+          </ul>
+          <form onSubmit={this.handleUserSubmit.bind(this)}>
+            <input ref="newUserName" placeholder="New player" />
+            <input type="submit" value="Add new player" />
+          </form>
+          { this.state.currentUser ? <p><button onClick={this.startGame}>All users submitted</button></p> : '' }
+        </div>
+      );
+    }
   }
 }
+
+export default withTracker(() => {
+  return {
+    players: Players.find({}).fetch(),
+  };
+})(Game);
