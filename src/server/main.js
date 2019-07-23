bca3712b1e6531e1633e66922e316c4b819bac1e
Tom Segarra <tomsegarra@gmail.com>
2019-07-23 09:52:25 -0700

    Clean up old games using cron

--- src/server/main.js
+++ src/server/main.js
@@ -3,6 +3,18 @@ import { Meteor } from 'meteor/meteor';
 import '../lib/collections.js';
 import '../lib/cards.js';
 
+function cleanUpGamesAndPlayers() {
+  var cutOff = moment().subtract(2, 'hours').toDate().getTime();
+
+  var numGamesRemoved = Games.remove({
+    createdAt: {$lt: cutOff}
+  });
+
+  var numPlayersRemoved = Players.remove({
+    createdAt: {$lt: cutOff}
+  });
+}
+
 Meteor.startup(() => {
   Games.remove({});
   Players.remove({});
@@ -135,3 +147,7 @@ Games.find({'state': 'settingUpNextRound'}).observeChanges({
     }});
   },
 });
+
+var MyCron = new Cron(60000);
+
+MyCron.addJob(5, cleanUpGamesAndPlayers);
